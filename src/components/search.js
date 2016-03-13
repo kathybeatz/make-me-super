6d12a19b3fd01e708f8f80f4ee3ad0299d62928f
Jenna Vuong <jennavuong@gmail.com>
2016-03-12 16:49:39 -0800

    added axios package

--- src/components/search.js
+++ src/components/search.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 export default class Search extends Component {
 
+  componentWillMount() {
+
+    // fetch all batman activities from db
+    
+  }
 
   render(){
     return (
       <div>
-        <h1>Hello!</h1>
+        <button className="btn btn-primary btn-lg button" type="button">Make Me Batman!</button>
       </div>
+
     )
   }
 }
