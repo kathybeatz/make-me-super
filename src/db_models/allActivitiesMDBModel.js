002fb5ae863e7b0220bcb7c0002ae75edf31e42e
Justin Bell <justinbell714@gmail.com>
2016-03-13 11:21:36 -0700

    Made modifications to mongoose schema

--- src/db_models/allActivitiesMDBModel.js
+++ src/db_models/allActivitiesMDBModel.js
@@ -2,12 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var allActivities = new Schema ({
-  Type: String
-  Title: String
-  Location: String //location is a reserved word, but also one of our fields. Looking in to workarounds/using 'locale'
-  Description: String
-  Link: String //Same situation as Location. Links is not reserved.
-
+  Type: String,
+  Title: String,
+  Locale: String, 
+  Description: String,
+  Links: String 
 
 });
 
