b61decdc025427b066f65ad3968221c43f984c35
Justin Bell <justinbell714@gmail.com>
2016-03-12 23:26:35 -0800

    Added schemas for mongoose and 3rd page of app

---
+++ src/db_models/allActivitiesMDBModel.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var allActivities = new Schema ({
+  Type: String
+  Title: String
+  Location: String //location is a reserved word, but also one of our fields. Looking in to workarounds/using 'locale'
+  Description: String
+  Link: String //Same situation as Location. Links is not reserved.
+
+
+});
+
+module.exports = mongoose.model("allActivities", allActivities);
