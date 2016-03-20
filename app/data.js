eab6b4365e9afc255bb25b940f8b4f9c6a20a677
Kathleen L <kathleeng.lopez@gmail.com>
2016-03-19 17:24:45 -0700

    Added demo data

---
+++ app/data.js
@@ -0,0 +1,104 @@
+const dummyData = {
+	{ name: 'Albrey',
+	image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/t/56b10dc962cd9416699645ce/1454443988362/?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Bianca',
+   	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/t/56b10e458259b562ca19c5aa/1454444106294/?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Jamil',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b0fba407eaa096f03e712f/56b0fbce40261d778a9447d4/1454439374960/jamil_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Claire',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b0fba407eaa096f03e712f/56b0fc1804426214620e8b33/1454439450733/claire_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Krystal',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b0fba407eaa096f03e712f/56b0fbf2cf80a1b35a628fe7/1454439411318/krystal_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Jazz',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b10eff7c65e4b78436f31d/56b10f0020c647701cab16c6/1454444289013/jazz_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Bosa',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b10eff7c65e4b78436f31d/56b10f00f85082c4982e212a/1454444288803/bosa_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Gabriel',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b10eff7c65e4b78436f31d/56b10f00f85082c4982e2131/1454444289006/gabriel_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Shanna',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b10eff7c65e4b78436f31d/56b10f2af8baf34c94848945/1454444331247/cutmypic+%281%29.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	{ name: 'Kristina',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b10eff7c65e4b78436f31d/56b10f44f8baf34c94848a2e/1454444356780/cutmypic+%283%29.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 },
+	 { name: 'Darryl',
+	  image: 'http://static1.squarespace.com/static/54d53828e4b0e1d1a7d75bbf/56b10eff7c65e4b78436f31d/56b11143f699bbbc08e4f739/1454450117452/darryl_round.png?format=300w',
+	  attributes: 
+	  	{ conjuring: 40,
+	  	  scribery: 30,
+	  	  empathy: 80,
+	  	  bathroomBreak: 60,
+	  	  luck: 70 }
+	 }
+};
+
+
+export default dummyData
