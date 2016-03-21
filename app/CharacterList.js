ab2434665f8362502330b4f48b6590c3cac0a38f
Colin McDonald <mickydwow@gmail.com>
2016-03-21 14:34:11 -0700

    Working on styling

---
+++ app/CharacterList.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class CharacterList extends Component {
+  render(){
+    const { characters, handleClick } = this.props
+    return (
+      <ul>
+        {this.props.characters.map((character, i) => 
+          <img className="character-thumbnail" onClick={e => handleClick(character)} key={i} src={character.image} />
+        )}
+      </ul>
+    )
+  }
+}
+
+CharacterList.propTypes = {
+  characters: PropTypes.array.isRequired,
+  handleClick: PropTypes.func.isRequired
+}
